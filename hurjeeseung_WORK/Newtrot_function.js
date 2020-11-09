d240982b6e1bec255b6f08720aa00e2b08c7452a
Jihye An <anjihye9526@gmail.com>
2020-11-09 16:04:00 +0900

    모바일 버전 글쓰 크기 조정

--- hurjeeseung_WORK/Newtrot_function.js
+++ hurjeeseung_WORK/Newtrot_function.js
@@ -1,3 +1,4 @@
+
 $(function() {
     $("#year").on("propertychange change input",function(){
         let yearval=$(this).val();
@@ -26,30 +27,49 @@ $(function() {
        for(var i=0;i<output.length;i++)output[i].style.height='auto';
        $(".output-letter").height( $(".output-letter").prop('scrollHeight'));
 
-
-       // 여기가 글씨 개수대로 글자크기 바꾸는 곳이야, 글자수는 숫자들 바꾸면 되고 글자크기는 맘대로바꿔!
-        if(content.length <= 6 && content.length>=0){
-        $("#input-letter").css("fontSize",'10rem');
-        }else if (content.length > 6 && content.length <=15 ){
-        $("#input-letter").css("fontSize",'8rem');
-        }
-        else if(content.length>15&&content.length<=28){
-        $("#input-letter").css('font-size','6rem');
-        }
-        else if(content.length>28){
-        $("#input-letter").css('font-size','4rem');
-        }
+       if($(window).width()>505){
+           chagefontsize('web',content.length);
+       }else{
+           chagefontsize('mobile',content.length);
+       }
+       
     });
-   
-
-
 
 });
 
+// 여기가 글씨 개수대로 글자크기 바꾸는 곳이야, 글자수는 숫자들 바꾸면 되고 글자크기는 맘대로바꿔!
+function chagefontsize(windowtype,length){
+    if(windowtype=='mobile'){                       //여기 모바일 일때
+        if(length <= 6 && length>=0){
+            $("#input-letter").css("fontSize",'3rem');
+            }else if (length > 6 && length <=15 ){
+            $("#input-letter").css("fontSize",'2.5rem');
+            }
+            else if(length>15&&length<=28){
+            $("#input-letter").css('font-size','2rem');
+            }
+            else if(length>28){
+            $("#input-letter").css('font-size','1.8rem');
+            }
+
+    }else if(windowtype=='web'){                // 여기 웹일때
+        if(length <= 6 && length>=0){
+            $("#input-letter").css("fontSize",'10rem');
+            }else if (length > 6 && length <=15 ){
+            $("#input-letter").css("fontSize",'8rem');
+            }
+            else if(length>15&&length<=28){
+            $("#input-letter").css('font-size','6rem');
+            }
+            else if(length>28){
+            $("#input-letter").css('font-size','4rem');
+            }
+    }
+
+}
 
 var ckwindow =window.matchMedia("screen and (min-width:500px) ");
 if(ckwindow.matches){
-
 $(document).mousemove(function(event){
     
     xPercent = event.pageX / $( '.first-part' ).width() * 100;
